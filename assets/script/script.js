f847884882455d81ddb5a98a1b5ea83de66a730b
AndreaBar94 <122469003+AndreaBar94@users.noreply.github.com>
2023-03-04 11:20:31 +0100

    animazione M

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,7 +2,7 @@ let navbar = document.querySelector('nav');
 let button = document.querySelector('.getStarted');
 
 window.onscroll = function() {
-    if (window.pageYOffset > 200) {
+    if (window.pageYOffset > 300) {
         navbar.classList.add('active')
         button.classList.add('active2')
     } else {
@@ -13,6 +13,20 @@ window.onscroll = function() {
 
 // ANIMAZIONE LETTERE
 
-let letter = document.querySelectorAll('svg')
-console.log(letter);
+let letters = document.querySelectorAll('.animatedM svg g')
 
+function selectRandom(x){
+    return x[Math.floor(Math.random() * x.length)];
+}
+
+// aggiungo classe per farle sparire
+function animateLetter(){
+    let switchTrue = selectRandom(Array.from(letters));
+    if(switchTrue.classList.contains('active3')){
+        switchTrue.classList.remove('active3');
+    }else{
+        switchTrue.classList.add('active3');
+    }
+};
+//attivo tutto
+setInterval(animateLetter, 2);
